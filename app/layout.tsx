5e9a715e5335b13890b27f4e837a81aa85e51c7b
Ralph <ecowapp@gmail.com>
2025-11-25 10:40:52 +0800

    seond commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
+import { ReactNode } from "react";
 import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 export const metadata = {
   title: "TESDA TTI Locations",
-  description: "Search TESDA Training and Testing Institutions (TTIs) and open them in Google Maps.",
+  description:
+    "Search TESDA Training and Testing Institutions (TTIs) and open them in Google Maps.",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-tesda-900 text-white">
         <Header />
         <main className="min-h-[calc(100vh-120px)] p-6 sm:p-10">{children}</main>
         <Footer />
